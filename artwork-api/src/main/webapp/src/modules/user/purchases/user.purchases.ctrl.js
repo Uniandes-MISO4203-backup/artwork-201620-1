57494decd7818827e6546f67f06545d441fa86bd
s.florez10 <s.florez10@MISO4203-10.virtual.uniandes.edu.co>
2016-10-19 17:26:41 -0500

    vista de compras de usuario implementadas

--- artwork-api/src/main/webapp/src/modules/user/purchases/user.purchases.ctrl.js
+++ artwork-api/src/main/webapp/src/modules/user/purchases/user.purchases.ctrl.js
@@ -8,7 +8,12 @@
 
     mod.controller('userPurchasesCtrl', ['$scope',
         function ($scope) {
-            
+            //Asignar resultado de compras de servicio
+            $scope.purchases=[{items:[{a:1},{a:2},{a:3},{a:4}]},{items:[{a:5},{a:6},{a:7},{a:8}]},{items:[{},{},{},{}]},{items:[{},{},{},{}]}, {items:[{},{},{},{}]}]; 
+            $scope.states = new Array($scope.purchases.length).fill(false);;
+            $scope.changeState = function(index){
+                $scope.states[index] = !$scope.states[index];
+            };
         }
     ]);
 })(window.angular);
