ad750c3603d1b4b7cf18760139ff3772aac899de
a.quintero10 <a.quintero10@MISO4203-21.virtual.uniandes.edu.co>
2016-10-21 02:49:38 -0500

    se crea servicio mis compras

--- artwork-logic/src/main/java/co/edu/uniandes/csw/artwork/entities/ItemEntity.java
+++ artwork-logic/src/main/java/co/edu/uniandes/csw/artwork/entities/ItemEntity.java
@@ -26,49 +26,14 @@ package co.edu.uniandes.csw.artwork.entities;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import co.edu.uniandes.csw.crud.spi.entity.BaseEntity;
-import java.math.BigInteger;
-import java.util.Collection;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import uk.co.jemos.podam.common.PodamExclude;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  * @generated
  */
 @Entity
 public class ItemEntity extends BaseEntity implements Serializable {
-
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- @Basic(optional = false)
- @Column(name = "ID")
- private Long id;
- @Size(max = 255)
- @Column(name = "NAME")
- private String name;
- @Column(name = "QTY")
- private BigInteger qty;
- @Column(name = "SHOPPINGCART")
- private Short shoppingcart;
- @ManyToMany(mappedBy = "itemEntityCollection")
- private Collection<PaymentEntity> paymentEntityCollection;
- @JoinColumn(name = "ARTWORK_ID", referencedColumnName = "ID")
- @ManyToOne
- private ArtworkEntity artworkId;
- @JoinColumn(name = "CLIENT_ID", referencedColumnName = "ID")
- @ManyToOne
- private ClientEntity clientId;
- @JoinColumn(name = "PRODUCT_ID", referencedColumnName = "ID")
- @ManyToOne
- private ProductEntity productId;
 
     private Long qty;
     
@@ -186,101 +151,4 @@ public class ItemEntity extends BaseEntity implements Serializable {
     public void setProduct(ProductEntity product) {
         this.product = product;
     }    
-
- public ItemEntity() {
- }
-
- public ItemEntity(Long id) {
-  this.id = id;
- }
-
- public Long getId() {
-  return id;
- }
-
- public void setId(Long id) {
-  this.id = id;
- }
-
- public String getName() {
-  return name;
- }
-
- public void setName(String name) {
-  this.name = name;
- }
-
- public BigInteger getQty() {
-  return qty;
- }
-
- public void setQty(BigInteger qty) {
-  this.qty = qty;
- }
-
- public Short getShoppingcart() {
-  return shoppingcart;
- }
-
- public void setShoppingcart(Short shoppingcart) {
-  this.shoppingcart = shoppingcart;
- }
-
- @XmlTransient
- public Collection<PaymentEntity> getPaymentEntityCollection() {
-  return paymentEntityCollection;
- }
-
- public void setPaymentEntityCollection(Collection<PaymentEntity> paymentEntityCollection) {
-  this.paymentEntityCollection = paymentEntityCollection;
- }
-
- public ArtworkEntity getArtworkId() {
-  return artworkId;
- }
-
- public void setArtworkId(ArtworkEntity artworkId) {
-  this.artworkId = artworkId;
- }
-
- public ClientEntity getClientId() {
-  return clientId;
- }
-
- public void setClientId(ClientEntity clientId) {
-  this.clientId = clientId;
- }
-
- public ProductEntity getProductId() {
-  return productId;
- }
-
- public void setProductId(ProductEntity productId) {
-  this.productId = productId;
- }
-
- @Override
- public int hashCode() {
-  int hash = 0;
-  hash += (id != null ? id.hashCode() : 0);
-  return hash;
- }
-
- @Override
- public boolean equals(Object object) {
-  // TODO: Warning - this method won't work in the case the id fields are not set
-  if (!(object instanceof ItemEntity)) {
-   return false;
-  }
-  ItemEntity other = (ItemEntity) object;
-  if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-   return false;
-  }
-  return true;
- }
-
- @Override
- public String toString() {
-  return "co.edu.uniandes.csw.artwork.entities.ItemEntity[ id=" + id + " ]";
- }
 }
